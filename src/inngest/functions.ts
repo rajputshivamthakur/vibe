362717fd840450703f2e6eaba36e7b026cee241b
Shivam Thakur <shivamthakur1924.com>
2025-07-30 19:40:10 +0530

    05: ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,14 +1,22 @@
+import { openai, gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    // Imagine this is a transcript step
-    await step.sleep("wait-a-moment", "10s");
+  async ({ event }) => {
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer.  You write readable, maintainable code. You write simple Next.js & React snippets",
+      model: gemini({ model: "gemma-3n-e4b-it" }),
+    });
 
-    // Imagine this is a summary step
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    return { output };
   }
 );
