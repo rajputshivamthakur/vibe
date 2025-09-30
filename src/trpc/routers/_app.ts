14a7bc261babcbf1e4a1c3cb53acd4633c18466f
Shivam Thakur <shivamthakur1924.com>
2025-09-30 09:24:55 +0530

    09: projects

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
-import { baseProcedure, createTRPCRouter } from "../init";
-import { messagesRoutrer } from "@/modules/messages/server/procedures";
+import { projectsRouter } from "@/modules/projects/server/procedures";
+import { createTRPCRouter } from "../init";
+import { messagesRouter } from "@/modules/messages/server/procedures";
 
 export const appRouter = createTRPCRouter({
-  messages: messagesRoutrer,
+  messages: messagesRouter,
+  projects: projectsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
